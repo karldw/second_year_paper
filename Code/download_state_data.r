cacb9a3aab40773cf3b10291fd1cbb2c2f012fd1
Karl Dunkle Werner <karldw@berkeley.edu>
2017-01-15 21:00:40 -0800

    Add states postgres database

--- Code/download_state_data.r
+++ Code/download_state_data.r
@@ -1,26 +1,36 @@
 
-DATA_DIR <- '../Data'
-stopifnot(dir.exists(DATA_DIR))
+# download_state_data.r
+# creates ../Data/us_state_gdp.rda
 
-population_data <- file.path(DATA_DIR, 'us_state_populations.rda')
-if (! file.exists(population_data)) {
+source('r_defaults.r')
+install_lazy(c('readr', 'reshape2', 'dplyr', 'curl'),
+             verbose = FALSE)
+library(readr)
+suppressPackageStartupMessages(library(dplyr))
+library(reshape2)
+suppressPackageStartupMessages(library(curl))
+# library(ensurer)
+DATA_DIR <- '../Data'
+
+
+download_pop_data <- function() {
+    stop('This function is no longer used.')
+    pop_data_file <- file.path(DATA_DIR, 'us_state_populations.rda')
     download.file('https://github.com/ropensci/historydata/blob/cccfd02844865c6e5b120f3325bfc24179e3d038/data/us_state_populations.rda',
-                  population_data,
-                  quiet = TRUE)
+                      pop_data_file,
+                      quiet = TRUE)
 }
 
 
-state_gdp <- file.path(DATA_DIR, 'us_state_gdp.rda')
-if (! file.exists(state_gdp)) {
-    library(readr)
-    library(dplyr)
-    library(reshape2)
+download_gdp_data <- function() {
+    gdp_data_file <- file.path(DATA_DIR, 'us_state_gdp.rda')
+
     tmp <- tempdir()
 
     # New GDP:
     new_gdp_url <- 'http://www.bea.gov/regional/zip/gsp/gsp_naics_all_PC.zip'
     new_gdp_zipfile <- file.path(tmp, 'new_gdp.zip')
-    download.file(new_gdp_url, new_gdp_zipfile, quiet=TRUE)
+    curl_download(new_gdp_url, new_gdp_zipfile)
     unzip(new_gdp_zipfile, exdir=tmp)
     new_gdp_csv <- file.path(tmp, 'gsp_naics_all_PC.csv')
     stopifnot(file.exists(new_gdp_csv))
@@ -40,28 +50,37 @@ if (! file.exists(state_gdp)) {
     'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii',
     'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
     'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
-    'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New
-    York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
-    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
-    'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming')
-    new_gdp_df <- read_csv(new_gdp_csv, col_types=cols_to_read) %>%
+    'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+    'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+    'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin',
+    'Wyoming')
+    stopifnot(length(states) == 51)  # because of DC
+    suppressWarnings(new_gdp_df <- read_csv(new_gdp_csv, col_types = cols_to_read))
+    new_gdp_df <- new_gdp_df %>%
         filter(GeoName %in% states) %>%
         rename(state=GeoName) %>%
         select(-GeoFIPS, -Region, -ComponentId, -ComponentName, -IndustryId,
                -IndustryClassification, -Description) %>%
         melt(id.vars='state', variable.name='year', value.name='gdp_pc') %>%
         as.tbl() %>%
-        mutate(year = as.integer(levels(year))[year])
+        mutate(year = as.integer(levels(year))[year],
+               gdp_method = 'NAICS (new)')
+    new_gdp_years <- unique(new_gdp_df$year)
+    expected_nrow <- length(states) * length(new_gdp_years)
+    stopifnot(tbl_has_rows(new_gdp_df), nrow(new_gdp_df) == expected_nrow)
+
 
     # Old GDP:
     old_gdp_url <- 'http://www.bea.gov/regional/zip/gsp/gsp_sic_all_PC.zip'
     old_gdp_zipfile <- file.path(tmp, 'ols_gdp.zip')
-    download.file(old_gdp_url, old_gdp_zipfile, quiet=TRUE)
+    curl_download(old_gdp_url, old_gdp_zipfile)
     unzip(old_gdp_zipfile, exdir=tmp)
     old_gdp_csv <- file.path(tmp, 'gsp_sic_all_PC.csv')
     stopifnot(file.exists(old_gdp_csv))
-    old_gdp_df <- read_csv(old_gdp_csv, col_types=cols_to_read,
-                           na=c('', '(NA)')) %>%
+    suppressWarnings(old_gdp_df <- read_csv(old_gdp_csv, col_types=cols_to_read,
+                                            na=c('', '(NA)')))
+    old_gdp_df <- old_gdp_df %>%
         filter(GeoName %in% states) %>%
         rename(state=GeoName) %>%
         select(-GeoFIPS, -Region, -ComponentId, -ComponentName, -IndustryId,
@@ -69,8 +88,25 @@ if (! file.exists(state_gdp)) {
         melt(id.vars='state', variable.name='year', value.name='gdp_pc') %>%
         as.tbl() %>%
         mutate(year = as.integer(levels(year))[year]) %>%
-        filter(year >= 1987)  # NA before that
+        filter(year >= 1987, ! year %in% new_gdp_years) %>% # NA before that
+        mutate(gdp_method = 'SIC (old)')
+    expected_nrow <- length(states) * length(unique(old_gdp_df$year))
+    stopifnot(tbl_has_rows(old_gdp_df), nrow(old_gdp_df) == expected_nrow)
 
     gdp_df <- bind_rows(old_gdp_df, new_gdp_df)
-    saveRDS(gdp_df, state_gdp)
+
+    saveRDS(gdp_df, gdp_data_file)
 }
+
+
+main <- function() {
+    stopifnot(dir.exists(DATA_DIR))
+
+    # Instead of this file, use the county-level data in parse_county_data.r and
+    # aggregate up to the state level.
+    # download_pop_data()
+
+    download_gdp_data()
+}
+
+main()
---
+++ Code/load_states_into_postgres.r
@@ -0,0 +1,120 @@
+
+# load_state_info_into_postgres.r
+# relies on ../Data/us_county_by_year_population.feather, which is the output of
+# parse_county_data.r
+
+source('r_defaults.r')
+
+install_lazy(c('RPostgreSQL', 'dplyr', 'ensurer'), verbose = FALSE)
+library(RPostgreSQL)
+library(ensurer)
+suppressPackageStartupMessages(library(dplyr))
+options(warn = 2)
+DATA_DIR <- '../Data'
+POSTGRES_DB <- 'second_year_paper'
+POSTGRES_TABLE <- 'states'
+
+DATA_TYPES <- c(state = 'char(2)', year = 'int2', population = 'int4', gdp_pc = 'int4',
+  gdp_method = 'text', state_fips = 'char(2)', state_full = 'text')
+
+
+STATES <- c(
+    'Alabama' = 'AL', 'Alaska' = 'AK', 'Arizona' = 'AZ', 'Arkansas' = 'AR',
+    'California' = 'CA', 'Colorado' = 'CO', 'Connecticut' = 'CT', 'Delaware' = 'DE',
+    'District of Columbia' = 'DC', 'Florida' = 'FL', 'Georgia' = 'GA', 'Hawaii' = 'HI',
+    'Idaho' = 'ID', 'Illinois' = 'IL', 'Indiana' = 'IN', 'Iowa' = 'IA', 'Kansas' = 'KS',
+    'Kentucky' = 'KY', 'Louisiana' = 'LA', 'Maine' = 'ME', 'Maryland' = 'MD',
+    'Massachusetts' = 'MA', 'Michigan' = 'MI', 'Minnesota' = 'MN', 'Mississippi' = 'MS',
+    'Missouri' = 'MO', 'Montana' = 'MT', 'Nebraska' = 'NE', 'Nevada' = 'NV',
+    'New Hampshire' = 'NH', 'New Jersey' = 'NJ', 'New Mexico' = 'NM', 'New York' = 'NY',
+    'North Carolina' = 'NC', 'North Dakota' = 'ND', 'Ohio' = 'OH', 'Oklahoma' = 'OK',
+    'Oregon' = 'OR', 'Pennsylvania' = 'PA', 'Rhode Island' = 'RI',
+    'South Carolina' = 'SC', 'South Dakota' = 'SD', 'Tennessee' = 'TN', 'Texas' = 'TX',
+    'Utah' = 'UT', 'Vermont' = 'VT', 'Virginia' = 'VA', 'Washington' = 'WA',
+    'West Virginia' = 'WV', 'Wisconsin' = 'WI', 'Wyoming' = 'WY'
+    )
+STATES_DF <- data_frame(state_full = names(STATES), state = unname(STATES))
+
+
+get_state_fips <- function() {
+    # Get a table of the names, abbreviations and fips codes.
+    pop_data_file <- file.path(DATA_DIR, 'us_county_by_year_population.rda')
+    stopifnot(file.exists(pop_data_file))
+
+    readRDS(pop_data_file) %>%
+        as.tbl() %>%
+        distinct(state, stname, .keep_all = FALSE) %>%  # explicit false
+        rename(state_fips = state, state_full = stname) %>%
+        left_join(STATES_DF, by = 'state_full') %>%
+        ensure(! anyNA(.)) %>%
+        ensure(nrow(.) == length(STATES)) %>%
+        return()
+}
+
+
+get_pop_data <- function() {
+    pop_data_file <- file.path(DATA_DIR, 'us_county_by_year_population.rda')
+    stopifnot(file.exists(pop_data_file))
+    state_fips_df <- get_state_fips()
+    pop_df <- readRDS(pop_data_file) %>%
+        as.tbl() %>%
+        rename(state_fips = state) %>%
+        left_join(state_fips_df, by = 'state_fips') %>%
+        select(state, year, population) %>%
+        group_by(state, year) %>%
+        # sum county pop to state-year
+        summarize(population = sum(population)) %>%
+        ensure_id_vars(state, year) %>%
+        ensure(! anyNA(.))
+
+    expected_nrow <- length(unique(pop_df$year)) * length(STATES)
+    stopifnot(nrow(pop_df) == expected_nrow)
+    return(pop_df)
+}
+
+
+get_gdp_data <- function() {
+    gdp_data_file <- file.path(DATA_DIR, 'us_state_gdp.rda')
+    stopifnot(file.exists(gdp_data_file))
+    state_fips_df <- get_state_fips()
+    readRDS(gdp_data_file) %>%
+        as.tbl() %>%
+        rename(state_full = state) %>%
+        left_join(state_fips_df, by = 'state_full') %>%
+        select(state, year, gdp_pc, gdp_method) %>%
+        ensure_id_vars(state, year) %>%
+        ensure(! anyNA(.)) %>%
+        return()
+}
+
+
+
+main <- function(verbose = TRUE) {
+    pop_df <- get_pop_data()
+    gdp_df <- get_gdp_data()
+    state_fips_df <- get_state_fips()
+    combined_df <- full_join(pop_df, gdp_df, by = c('state', 'year')) %>%
+        left_join(state_fips_df, by = 'state')
+        #
+    auction_years_df <- combined_df %>%
+        filter(between(year, 2002, 2014)) %>%
+        ensure(! anyNA(.)) %>%
+        as.data.frame()  # as.data.frame because dbWriteTable doesn't handle tbls
+
+    con <- dbConnect("PostgreSQL", dbname = POSTGRES_DB)
+    if (DBI::dbExistsTable(con, POSTGRES_TABLE)) {
+        if (verbose) {
+            message('Deleting existing table.')
+        }
+        DBI::dbRemoveTable(con, POSTGRES_TABLE)
+    }
+    # returns false if unsuccessful
+    successful_write <- DBI::dbWriteTable(con, POSTGRES_TABLE, auction_years_df,
+                                          field.types = DATA_TYPES, row.names = FALSE)
+    stopifnot(successful_write)
+    pg_add_primary_key(con, POSTGRES_TABLE, c('state', 'year'))
+    DBI::dbDisconnect(con)
+}
+
+# Run things:
+# df <- main()
