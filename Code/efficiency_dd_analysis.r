310e4cea8727691857f861e72edbf615eb8610e6
Karl Dunkle Werner <karldw@berkeley.edu>
2017-01-25 14:34:32 -0800

    Begin efficiency plots

--- Code/efficiency_dd_analysis.r
+++ Code/efficiency_dd_analysis.r
@@ -11,12 +11,26 @@ POSTGRES_DB <- 'second_year_paper'
 POSTGRES_CLEAN_TABLE <- 'auctions_cleaned'
 POSTGRES_VIN_DECODER_TABLE <- 'vin_decoder'
 if (! exists('con')) {
-    con <- src_postgres(POSTGRES_DB)
+    pg_user <- Sys.info()[["user"]] %>% tolower()
+    con <- src_postgres(POSTGRES_DB, user = pg_user, password = pg_user)
 }
-auctions <-  tbl(con, POSTGRES_CLEAN_TABLE)
-states <- tbl(con, 'states')
-vin_decoder <- tbl(con, POSTGRES_VIN_DECODER_TABLE)
+auctions    <- tbl(con, POSTGRES_CLEAN_TABLE)
+states      <- tbl(con, 'states')
+vin_decoder <- tbl(con, POSTGRES_VIN_DECODER_TABLE)  # uniquely identified by vin_pattern
 
-if (! exists('auctions_with_mpg')) {
-    # auctions_with_mpg <- 
-}
+#
+# # testing
+# auctions_nrow <- force_nrow(auctions)
+# auctions_unmatched <- dplyr::left_join(auctions, vin_decoder, by = 'vin_pattern') %>%
+#     select(msrp) %>%
+#     filter(is.na(msrp)) %>%
+#     force_nrow()
+# print(paste0(100 * auctions_unmatched / auctions_nrow, "% of sales unmatched."))
+
+state_day_mpg_avg <- auctions %>%
+    filter(! is.na(buy_state)) %>%
+    select(sale_date, buy_state, vin_pattern) %>%
+    inner_join(vin_decoder, by = 'vin_pattern')  %>%
+    select(sale_date, buy_state, combined) %>%
+    group_by(sale_date, buy_state) %>%
+    summarize(combined_mpg = mean(combined), count = n())
