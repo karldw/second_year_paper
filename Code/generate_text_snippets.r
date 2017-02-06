797a56e35213827d465aed72818b6724122483e0
Karl Dunkle Werner <karldw@berkeley.edu>
2017-02-05 17:04:31 -0800

    Add original row count

--- Code/generate_text_snippets.r
+++ Code/generate_text_snippets.r
@@ -11,11 +11,13 @@ suppressPackageStartupMessages(library(dplyr))
 
 POSTGRES_DB <- 'second_year_paper'
 POSTGRES_CLEAN_TABLE <- 'auctions_cleaned'
+POSTGRES_ORIG_TABLE <- 'all_years_all_sales'
+
 if (! exists('con')) {
     pg_user <- Sys.info()[["user"]] %>% tolower()
     con <- src_postgres(POSTGRES_DB, user = pg_user, password = pg_user)
 }
-# orig_data <- tbl(con, POSTGRES_ORIG_TABLE)
+orig_data <- tbl(con, POSTGRES_ORIG_TABLE)
 auctions <-  tbl(con, POSTGRES_CLEAN_TABLE)
 # states <- tbl(con, 'states')
 
@@ -26,3 +28,7 @@ auctions %>% count() %>%
 # Alaskan buy count:
 auctions %>% filter(buy_state == 'AK') %>% count() %>%
     make_snippet('auctions_cleaned_alaska_obs_count.tex', sig_figs = 20)
+
+# Raw sale count (properly, should include bad date filters, but these are zero)
+orig_data %>% count() %>%
+    make_snippet('auctions_uncleaned_total_obs_count.tex', sig_figs = 20)
