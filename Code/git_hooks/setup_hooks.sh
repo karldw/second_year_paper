fa55f3cabc0bf8e36bc97e00737682e0557235cc
Karl Dunkle Werner <karldw@berkeley.edu>
2017-02-22 07:23:00 -0800

    Fix git hooks script

--- Code/git_hooks/setup_hooks.sh
+++ Code/git_hooks/setup_hooks.sh
@@ -4,4 +4,6 @@ set -euf -o pipefail
 
 # See directions in http://mirrors.ctan.org/macros/latex/contrib/gitinfo2/gitinfo2.pdf
 
-cp -pi post-* ../git/hooks/
+cp -pi post-checkout  ../../.git/hooks/
+cp -pi post-commit    ../../.git/hooks/
+cp -pi post-merge     ../../.git/hooks/
